f8eea1af8139fcaf3419df9320e18ba78e952a62
Teepan <paartheepan94@gmail.com>
2024-11-01 10:56:34 +0100

    fix: add error handling

--- add.js
+++ add.js
@@ -1,3 +1,9 @@
 const add = (a, b) => {
+  if (isNaN(a)) {
+    throw new Error('the first parameter should be of type number')
+  }
+  if (isNaN(b)) {
+    throw new Error('the second parameter should be of type number')
+  }
   return a + b;
 }
